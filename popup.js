1e2cf704d4b26d82bf392e982ddb8564df32767e
Pratyush <connect2pratyush@gmail.com>
2022-08-23 13:04:10 +0530

    init popup

---
+++ popup.js
@@ -0,0 +1,6 @@
+// popup page
+
+var b = document.getElementById("rate");
+b.onclick = ()=>{
+    window.open('https://chrome.google.com/webstore/detail/vit-captcha-solver/balpfhmdaaahhppiijcgaemeoeojejam/reviews')
+}
