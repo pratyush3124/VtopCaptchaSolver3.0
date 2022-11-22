fd4b682dbcda1b76e3d906d0f6840b73f9dce390
Pratyush <connect2pratyush@gmail.com>
2022-11-23 01:24:20 +0530

    copy to clipboard

--- popup.js
+++ popup.js
@@ -2,5 +2,10 @@
 
 var b = document.getElementById("rate");
 b.onclick = ()=>{
-    window.open('https://chrome.google.com/webstore/detail/vit-captcha-solver/balpfhmdaaahhppiijcgaemeoeojejam/reviews')
+  window.open('https://chrome.google.com/webstore/detail/vit-captcha-solver/balpfhmdaaahhppiijcgaemeoeojejam/reviews')
+}
+
+var copy = document.getElementById("copy");
+copy.onclick = ()=>{
+  navigator.clipboard.writeText("https://chrome.google.com/webstore/detail/vtop-captcha-solver/balpfhmdaaahhppiijcgaemeoeojejam");
 }
